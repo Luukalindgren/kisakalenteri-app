0a180f019ef537950e3cbc5fa36c780639fd3f97
Luuka Lindgren <luslin@utu.fi>
2023-06-14 11:17:17 +0300

    Custom hook for data fetching

---
+++ hook/useFetch.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useFetch = (query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: "https://competition-calendar-server.onrender.com/competitions",
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("Tapahtui virhe");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+    return { data, isLoading, error, refetch };
+};
+export default useFetch;
