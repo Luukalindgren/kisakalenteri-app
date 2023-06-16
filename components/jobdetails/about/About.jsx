f56e4c6be6db14780a3ebc0b7b341760612f2298
Luuka Lindgren <luslin@utu.fi>
2023-06-16 12:34:21 +0300

    Moved link to competition page "About" -> "Footer"

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
 
 import styles from "./about.style";
 
-const About = ({ title, location, time, ID }) => {
-  const handleClick = () => {
-    //Link to competition page: `https://discgolfmetrix.com/${ID}`
-  };
-
+const About = ({ title, location, time }) => {
   return (
     <View>
       <View style={styles.container}>
@@ -16,11 +12,6 @@ const About = ({ title, location, time, ID }) => {
           <Text style={styles.contextText}>Rata: {location}</Text>
           <Text style={styles.contextText}>Päivä ja aika: {time}</Text>
         </View>
-      </View>
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.btn} onPress={handleClick}>
-          <Text style={styles.btnText}>Ilmoittaudu</Text>
-        </TouchableOpacity>
       </View>
     </View>
   );
